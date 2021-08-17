39bab3425561c4f5e06345a0b051087e303410d8
Khushaal2008 <75684230+Khushaal2008@users.noreply.github.com>
2021-08-17 21:14:13 +0530

    Add files via upload

---
+++ screens/IssLocationScreen.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class IssLocationScreen extends React.Component{
+    render(){
+        return(
+            <View
+            style={{
+                flex: 1,
+                justifyContent: "center",
+                alignItems: "center"
+            }}
+            >
+                <Text>Iss Location Screen</Text>
+            </View>
+        )
+    }
+}
